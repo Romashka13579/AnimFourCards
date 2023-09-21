b1e52e1d7530d2ab55ff93af3b516ad13fb77a1c
Romashka <rroma13579@gmail.com>
2023-09-21 23:28:23 +0200

    some kind of moving

--- js/script.js
+++ js/script.js
@@ -213,6 +213,7 @@ rangeInputs.forEach(rangeInput => {
 
 
 var fun3dCubic1part = document.querySelector('.fun-3d-cubic-1part');
+var fun3dCubic2part = document.querySelector('.fun-3d-cubic-2part');
 var funCubicUp = document.querySelector('.fun-cubic-button-up');
 var funCubicDown = document.querySelector('.fun-cubic-button-down');
 
@@ -220,7 +221,9 @@ funCubicUp.addEventListener('click', () => {
     var i = parseInt(fun3dCubic1part.dataset.index);
     i++;
     fun3dCubic1part.dataset.index = i;
-    fun3dCubic1part.style.transform = `rotateX(${90 * (2-i)}deg) translateZ(150px)` // perspective(2000px) works, but not the way I want, need to understand why
+    fun3dCubic2part.dataset.index = i;
+    fun3dCubic1part.style.transform = ` translateZ(300px) translateY(150px) rotateX(${90 * (2-i)}deg)` // perspective(2000px) works, but not the way I want, need to understand why
+    fun3dCubic2part.style.transform = `translateZ(-150px) rotateX(${90 * (2-i)}deg)` // perspective(2000px) works, but not the way I want, need to understand why
 });
 
 funCubicDown.addEventListener('click', () => {
