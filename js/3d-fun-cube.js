35c75b0a629f6eb929825a6776cceb829845df30
Romashka <rroma13579@gmail.com>
2023-09-24 19:01:16 +0200

    I think I will delete the code an start again

--- js/3d-fun-cube.js
+++ js/3d-fun-cube.js
@@ -5,11 +5,11 @@ var funCubicDown = document.querySelector('.fun-cubic-button-down');
 
 funCubicUp.addEventListener('click', () => {
     var i = parseInt(fun3dCubic1part.dataset.index);
-    i++;
+    i = i <= 4? i++ : i=1;
     fun3dCubic1part.dataset.index = i;
     fun3dCubic2part.dataset.index = i;
     fun3dCubic1part.style.transform = `perspective(2000px) translateZ(${150 * (3-i)}px) translateY(${150 * (4-i)}px) rotateX(${90 * (2-i)}deg)` // perspective(2000px) works, but not the way I want, need to understand why
-    fun3dCubic2part.style.transform = `perspective(2000px) rotateX(${90 * (1-i)}deg) translateZ(${150 * (2-i)}px)` // perspective(2000px) works, but not the way I want, need to understand why
+    fun3dCubic2part.style.transform = `perspective(2000px) rotateX(${90 * (3-i)}deg)` // perspective(2000px) works, but not the way I want, need to understand why
 });
 
 funCubicDown.addEventListener('click', () => {
