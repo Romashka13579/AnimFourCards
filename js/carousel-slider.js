4155f59531e7bbf0550a1c6375fffc3751067154
Romashka <rroma13579@gmail.com>
2023-09-25 14:06:50 +0200

    dont know what is a problem but buttons dont work

--- js/carousel-slider.js
+++ js/carousel-slider.js
@@ -1,7 +1,25 @@
 var carouselSliderCard = document.querySelectorAll('.carousel-slider-card');
-var i=0;
+var i = 0;
+var k = 0;
 
 carouselSliderCard.forEach(card => {
     i++;
-    card.style.transform = `rotateY(${60 * (i-1)}deg) translateZ(270px)`; 
+    card.style.transform = `rotateY(${60 * (i - 1)}deg) translateZ(270px)`;
+});
+
+var carouselButtonLeft = document.querySelector('.carousel-button-left');
+var carouselButtonRight = document.querySelector('.carousel-button-right');
+
+carouselButtonLeft.addEventListener('click', () => {
+    carouselSliderCard.forEach(card => {
+        k++;
+        card.style.transform = `rotateY(${60 * i}deg) translateZ(270px)`;
+    });
+});
+
+carouselButtonRight.addEventListener('click', () => {
+    carouselSliderCard.forEach(card => {
+        k--;
+        card.style.transform = `rotateY(${60 * i}deg) translateZ(270px)`;
+    });
 });
