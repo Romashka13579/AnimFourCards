5a7561d44a6615f9efa39bf7244704a937c5319e
Romashka <rroma13579@gmail.com>
2023-09-25 15:42:28 +0200

    carousel is working

--- js/carousel-slider.js
+++ js/carousel-slider.js
@@ -5,21 +5,26 @@ var k = 0;
 carouselSliderCard.forEach(card => {
     i++;
     card.style.transform = `rotateY(${60 * (i - 1)}deg) translateZ(270px)`;
+    card.style.transition = "1000ms";
 });
 
 var carouselButtonLeft = document.querySelector('.carousel-button-left');
 var carouselButtonRight = document.querySelector('.carousel-button-right');
 
 carouselButtonLeft.addEventListener('click', () => {
+    k++;
+    i = 0;
     carouselSliderCard.forEach(card => {
-        k++;
-        card.style.transform = `rotateY(${60 * i}deg) translateZ(270px)`;
+        i++;
+        card.style.transform = `rotateY(${60 * (i - 1 + k)}deg) translateZ(270px)`;
     });
 });
 
 carouselButtonRight.addEventListener('click', () => {
+    k--;
+    i = 0;
     carouselSliderCard.forEach(card => {
-        k--;
-        card.style.transform = `rotateY(${60 * i}deg) translateZ(270px)`;
+        i++;
+        card.style.transform = `rotateY(${60 * (i - 1 + k)}deg) translateZ(270px)`;
     });
 });
