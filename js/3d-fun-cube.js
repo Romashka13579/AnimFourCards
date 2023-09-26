205dd03d71b8a0a4e066daa95d5564123cd6fc53
Romashka <rroma13579@gmail.com>
2023-09-26 15:53:38 +0200

    have to create a formule for calculating all of the flips

--- js/3d-fun-cube.js
+++ js/3d-fun-cube.js
@@ -1,6 +1,9 @@
 var fun3dCubicpart = document.querySelectorAll('.fun-3d-cubic-part');
+var fun3dCubicparthorizontal = document.querySelectorAll('.fun-3d-cubic-part-horizontal');
 var funCubicUp = document.querySelector('.fun-cubic-button-up');
 var funCubicDown = document.querySelector('.fun-cubic-button-down');
+var funCubicLeft = document.querySelector('.fun-cubic-button-left');
+var funCubicRight = document.querySelector('.fun-cubic-button-right');
 
 funCubicUp.addEventListener('click', () => {
     fun3dCubicpart.forEach(cubicPart => {
@@ -12,8 +15,28 @@ funCubicUp.addEventListener('click', () => {
 });
 
 funCubicDown.addEventListener('click', () => {
-    var i = parseInt(fun3dCubic1part.dataset.index);
-    i--;
-    fun3dCubic1part.dataset.index = i;
-    fun3dCubic1part.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) translateZ(150px)`
+    fun3dCubicpart.forEach(cubicPart => {
+        var i = parseInt(cubicPart.dataset.index);
+        i--;
+        cubicPart.dataset.index = i;
+        cubicPart.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) translateZ(150px)` 
+    }); 
+});
+
+funCubicLeft.addEventListener('click', () => {
+    fun3dCubicparthorizontal.forEach(cubicPart => {
+        var i = parseInt(cubicPart.dataset.index2);
+        i++;
+        cubicPart.dataset.index2 = i;
+        cubicPart.style.transform = `perspective(700px) rotateY(${90 * (i)}deg) translateZ(150px)` 
+    }); 
+});
+
+funCubicRight.addEventListener('click', () => {
+    fun3dCubicparthorizontal.forEach(cubicPart => {
+        var i = parseInt(cubicPart.dataset.index2);
+        i--;
+        cubicPart.dataset.index2 = i;
+        cubicPart.style.transform = `perspective(700px) rotateY(${90 * (i)}deg) translateZ(150px)` 
+    }); 
 });
