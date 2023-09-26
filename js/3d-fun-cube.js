2c4ff9e5fac2470d9a4e2bc466f7a33d5c285fb5
Romashka <rroma13579@gmail.com>
2023-09-26 16:01:08 +0200

    feels kinda hard

--- js/3d-fun-cube.js
+++ js/3d-fun-cube.js
@@ -1,5 +1,4 @@
 var fun3dCubicpart = document.querySelectorAll('.fun-3d-cubic-part');
-var fun3dCubicparthorizontal = document.querySelectorAll('.fun-3d-cubic-part-horizontal');
 var funCubicUp = document.querySelector('.fun-cubic-button-up');
 var funCubicDown = document.querySelector('.fun-cubic-button-down');
 var funCubicLeft = document.querySelector('.fun-cubic-button-left');
@@ -8,35 +7,48 @@ var funCubicRight = document.querySelector('.fun-cubic-button-right');
 funCubicUp.addEventListener('click', () => {
     fun3dCubicpart.forEach(cubicPart => {
         var i = parseInt(cubicPart.dataset.index);
+        var i2 = parseInt(cubicPart.dataset.index2);
         i++;
+        i2++;
         cubicPart.dataset.index = i;
-        cubicPart.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) translateZ(150px)` 
+        cubicPart.dataset.index2 = i2;
+        console.log(i, i2);
+        cubicPart.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) rotateY(${90 * (i2)}deg) translateZ(150px)` 
     }); 
 });
 
 funCubicDown.addEventListener('click', () => {
     fun3dCubicpart.forEach(cubicPart => {
         var i = parseInt(cubicPart.dataset.index);
-        i--;
+        var i2 = parseInt(cubicPart.dataset.index2);
+        i++;
+        i2++;
         cubicPart.dataset.index = i;
-        cubicPart.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) translateZ(150px)` 
+        cubicPart.dataset.index2 = i2;
+        cubicPart.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) rotateY(${90 * (i2)}deg) translateZ(150px)` 
     }); 
 });
 
 funCubicLeft.addEventListener('click', () => {
-    fun3dCubicparthorizontal.forEach(cubicPart => {
-        var i = parseInt(cubicPart.dataset.index2);
+    fun3dCubicpart.forEach(cubicPart => {
+        var i = parseInt(cubicPart.dataset.index);
+        var i2 = parseInt(cubicPart.dataset.index2);
         i++;
-        cubicPart.dataset.index2 = i;
-        cubicPart.style.transform = `perspective(700px) rotateY(${90 * (i)}deg) translateZ(150px)` 
+        i2++;
+        cubicPart.dataset.index = i;
+        cubicPart.dataset.index2 = i2;
+        cubicPart.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) rotateY(${90 * (i2)}deg) translateZ(150px)` 
     }); 
 });
 
 funCubicRight.addEventListener('click', () => {
-    fun3dCubicparthorizontal.forEach(cubicPart => {
-        var i = parseInt(cubicPart.dataset.index2);
-        i--;
-        cubicPart.dataset.index2 = i;
-        cubicPart.style.transform = `perspective(700px) rotateY(${90 * (i)}deg) translateZ(150px)` 
+    fun3dCubicpart.forEach(cubicPart => {
+        var i = parseInt(cubicPart.dataset.index);
+        var i2 = parseInt(cubicPart.dataset.index2);
+        i++;
+        i2++;
+        cubicPart.dataset.index = i;
+        cubicPart.dataset.index2 = i2;
+        cubicPart.style.transform = `perspective(700px) rotateX(${90 * (i)}deg) rotateY(${90 * (i2)}deg) translateZ(150px)` 
     }); 
 });
