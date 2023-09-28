2cc9f36f7c719ecd20160151bebbf5c352c4aeb2
Romashka <rroma13579@gmail.com>
2023-09-28 15:21:58 +0200

    flipping is  done

---
+++ js/dog-flipping.js
@@ -0,0 +1,8 @@
+var dogFlippingCards = document.querySelectorAll('.dog-flipping-card');
+
+dogFlippingCards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.dataset.flipped = card.dataset.flipped == "0" ? 1:0;
+        card.style.transform = `perspective(650px) rotateX(${180*parseInt(card.dataset.flipped)}deg)`;
+    });
+});
