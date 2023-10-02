5a5e1951d7f2ae6cfa3ddd3a1b85ef6b153e694a
Romashka <rroma13579@gmail.com>
2023-10-02 12:11:02 +0200

    now only one card can be flipped at a time

--- js/dog-flipping.js
+++ js/dog-flipping.js
@@ -2,6 +2,10 @@ var dogFlippingCards = document.querySelectorAll('.dog-flipping-sandwich');
 
 dogFlippingCards.forEach(card => {
     card.addEventListener('click', () => {
+        dogFlippingCards.forEach(card => {
+            card.dataset.flipped = 0;
+            card.style.transform = `perspective(650px) rotateX(${180*parseInt(card.dataset.flipped)}deg)`;
+        });
         card.dataset.flipped = card.dataset.flipped == "0" ? 1:0;
         card.style.transform = `perspective(650px) rotateX(${180*parseInt(card.dataset.flipped)}deg)`;
     });
