bec27d604fd2719c9abdaaec2f210691ac7b1464
Romashka <rroma13579@gmail.com>
2023-10-02 13:50:10 +0200

    proper flipping

--- js/dog-flipping.js
+++ js/dog-flipping.js
@@ -2,11 +2,16 @@ var dogFlippingCards = document.querySelectorAll('.dog-flipping-sandwich');
 
 dogFlippingCards.forEach(card => {
     card.addEventListener('click', () => {
-        dogFlippingCards.forEach(card => {
-            card.dataset.flipped = 0;
-            card.style.transform = `perspective(650px) rotateX(${180*parseInt(card.dataset.flipped)}deg)`;
+        dogFlippingCards.forEach(card1 => {
+            if(card!=card1){
+            card1.dataset.flipped = 0;
+            card1.style.transform = `perspective(650px) rotateX(${180 * parseInt(card1.dataset.flipped)}deg)`;
+            console.log(card, card1);
+            }
+            else{
+                card.dataset.flipped = card.dataset.flipped == "0" ? 1 : 0;
+                card.style.transform = `perspective(650px) rotateX(${180 * parseInt(card.dataset.flipped)}deg)`;
+            }
         });
-        card.dataset.flipped = card.dataset.flipped == "0" ? 1:0;
-        card.style.transform = `perspective(650px) rotateX(${180*parseInt(card.dataset.flipped)}deg)`;
     });
 });
