fa4e0bec8721de44372441595f008d849d9bf846
Romashka <rroma13579@gmail.com>
2023-10-04 08:26:29 +0200

    testing events

--- js/postanim-dragging-cards.js
+++ js/postanim-dragging-cards.js
@@ -1,21 +1,24 @@
 cards = document.querySelectorAll('.postanim-dragging-card');
 
 cards.forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.dataset.over = "1";
+    card.addEventListener('mousedown', () => {
+        card.dataset.down = "1";
     });
-    card.addEventListener('mouseout', () => {
-        card.dataset.over = "0";
-    });
-    card.addEventListener("drag", (event) => {
-        var x = event.clientX;
-        var y = event.clientY;
-        card.dataset.dragged = "1";
-        console.log("1");
-        if(card.dataset.dragging == "1" && card.dataset.over == "1"){
-            card.style.left = `${x - 100}px`;
-            card.style.top = `${y - 100}px`;
+    card.addEventListener("mousemove", (event) => {
+        if(card.dataset.down == "1"){
+            card.dataset.dragged = "1";
+            var x = event.clientX;
+            var y = event.clientY;
+            console.log(x, y);
+            if(card.dataset.dragged == "1"){
+                card.style.left = `${x - 100}px`;
+                card.style.top = `${y - 100}px`;
+                console.log(`${x - 100}px`);
+            }
         }
+    });
+    card.addEventListener('mouseup', () => {
+        card.dataset.dragged = "0";
     });
 });
 
