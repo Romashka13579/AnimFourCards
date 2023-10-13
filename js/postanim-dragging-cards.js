8831040242693816ca7255ca205ace363f072f9a
Romashka <rroma13579@gmail.com>
2023-10-13 19:28:48 +0200

    progress in dragging, it was actually much easier

--- js/postanim-dragging-cards.js
+++ js/postanim-dragging-cards.js
@@ -4,17 +4,18 @@ var Area = document.querySelector('.postanim-dragging-cards');
 cards.forEach(card => {
     card.addEventListener('mousedown', (event) => {
         card.dataset.down = "1";
-        card.dataset.left = event.clientX - card.getBoundingClientRect().left;
-        card.dataset.top = event.clientY - card.getBoundingClientRect().top;
+        var left = event.clientX - card.getBoundingClientRect().left;
+        var top = event.clientY - card.getBoundingClientRect().top;
     });
-    Area.addEventListener("mousemove", (event) => {
+    window.addEventListener("mousemove", (event) => {
         if(card.dataset.down == "1"){
             card.dataset.dragged = "1";
             var x = event.clientX;
             var y = event.clientY;
             if(card.dataset.dragged == "1"){
-                card.style.left = `${x - 200}px`;
-                card.style.top = `${y - 100}px`;
+                card.style.position = "absolute";
+                card.style.left = `${x - (card.offsetWidth)/2}px`;
+                card.style.top = `${y - (card.offsetHeight)/2}px`;
                 console.log(y - (y - parseInt(card.dataset.top)));
             }
         }
