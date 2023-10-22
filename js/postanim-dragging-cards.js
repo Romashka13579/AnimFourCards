096a1380e6a1359f04e13cbce235b193c9291255
Romashka <rroma13579@gmail.com>
2023-10-22 22:19:03 +0200

    also try to do it through drag events

--- js/postanim-dragging-cards.js
+++ js/postanim-dragging-cards.js
@@ -7,32 +7,39 @@ clone.style.display = "none";
 clone.style.position = "absolute";
 
 cards.forEach(card => {
-    card.addEventListener('mousedown', () => {
-        card.dataset.down = "1";
-        clone.animate({
-            left: `${card.getBoundingClientRect().left}px`,
-            top: `${window.scrollY + card.getBoundingClientRect().top}px`
-        }, { duration: 0, fill: "forwards" });
-    });
-    document.addEventListener("mousemove", (event) => {
-        if(card.dataset.down == "1"){
-            card.dataset.dragged = "1";
-            var x = event.clientX;
-            var y = event.clientY;
-            if(card.dataset.dragged == "1"){
-                clone.style.display = "block";
-                clone.animate({
-                    left: `${x - (clone.offsetWidth)/2}px`,
-                    top: `${y - (clone.offsetHeight)/2 + window.scrollY}px`
-                }, { duration: 500, fill: "forwards" });
-            }
-        }
-    });
-    window.addEventListener('mouseup', () => {
-        card.dataset.dragged = "0";
-        card.dataset.down = "0";
-        clone.style.display = "none";
-        clone.style.transition = "none";
-    });
+    // card.addEventListener('mousedown', () => {
+    //     card.dataset.down = "1";
+    //     clone.animate({
+    //         left: `${card.getBoundingClientRect().left}px`,
+    //         top: `${window.scrollY + card.getBoundingClientRect().top}px`
+    //     }, { duration: 0, fill: "forwards" });
+    // });
+    // document.addEventListener("mousemove", (event) => {
+    //     if(card.dataset.down == "1"){
+    //         card.dataset.dragged = "1";
+    //         var x = event.clientX;
+    //         var y = event.clientY;
+    //         if(card.dataset.dragged == "1"){
+    //             clone.style.display = "block";
+    //             clone.animate({
+    //                 left: `${x - (clone.offsetWidth)/2}px`,
+    //                 top: `${y - (clone.offsetHeight)/2 + window.scrollY}px`
+    //             }, { duration: 500, fill: "forwards" });
+    //         }
+    //     }
+    // });
+    // window.addEventListener('mouseup', () => {
+    //     card.dataset.dragged = "0";
+    //     card.dataset.down = "0";
+    //     clone.style.display = "none";
+    //     clone.style.transition = "none";
+    // });
+    card.addEventListener('drag', () => {
+        console.log("abeba");
+        card.classList.add('dragged');
+    })
+    card.addEventListener('dragend', () => {
+        console.log("abeba2");
+    })
 });
 
