6db2becf231a60aa8df3bd008810789fe34f15f3
Romashka <rroma13579@gmail.com>
2023-11-06 11:52:10 +0100

    small changes

--- js/scrolling-background.js
+++ js/scrolling-background.js
@@ -11,8 +11,8 @@ window.addEventListener('scroll', () => {
     const scrollTopPosition = document.documentElement.scrollTop;
 
     for (let i = 0; i < scrollingBackgroundParts.length; i++) {
-        if(i < 3){scrollingBackgroundParts[i].style.opacity = `${(backTop - lastScrollTop + backheight)/backheight}`;}
-        else{scrollingBackgroundParts[i].style.opacity = `${(lastScrollTop - backTop - backheight)/backheight}`;}
+        if(i < 3){scrollingBackgroundParts[i].style.opacity = `${(backTop - lastScrollTop + backheight)/(2*backheight) + 0.5}`;}
+        else{scrollingBackgroundParts[i].style.opacity = `${(lastScrollTop - backTop - backheight)/(2*backheight) + 0.5}`;}
         if(lastScrollTop >= backTop){
             scrollingBackgroundParts[1].style.top = `${(lastScrollTop - backTop)/2}px`;
             scrollingBackgroundParts[2].style.top = `${(lastScrollTop - backTop)/3}px`;
