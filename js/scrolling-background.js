af584eb2e2d0b1cba4880b7fb5ce376a19ee9117
Romashka <rroma13579@gmail.com>
2023-11-06 12:11:02 +0100

    still some details need to be changed

--- js/scrolling-background.js
+++ js/scrolling-background.js
@@ -3,22 +3,26 @@ var scrollingBackground = document.querySelector('.scrolling-background');
 var scrollingBackgroundMain = document.querySelector('.scrolling-background-main');
 
 let lastScrollTop = document.documentElement.scrollTop;
-let firstScrollTop = document.documentElement.scrollTop;
-let backTop = scrollingBackground.offsetTop;
-let backheight = scrollingBackgroundMain.getBoundingClientRect().height;
+let backTop = scrollingBackground.offsetTop; // position of scrolling background in document = 7000+px
+let backheight = scrollingBackgroundMain.getBoundingClientRect().height; // height of the block = 100vh
 
 window.addEventListener('scroll', () => {
     const scrollTopPosition = document.documentElement.scrollTop;
 
-    for (let i = 0; i < scrollingBackgroundParts.length; i++) {
-        if(i < 3){scrollingBackgroundParts[i].style.opacity = `${(backTop - lastScrollTop + backheight)/(2*backheight) + 0.5}`;}
-        else{scrollingBackgroundParts[i].style.opacity = `${(lastScrollTop - backTop - backheight)/(2*backheight) + 0.5}`;}
-        if(lastScrollTop >= backTop){
-            scrollingBackgroundParts[1].style.top = `${(lastScrollTop - backTop)/2}px`;
-            scrollingBackgroundParts[2].style.top = `${(lastScrollTop - backTop)/3}px`;
-            scrollingBackgroundParts[4].style.top = `${(backTop - lastScrollTop + 2*backheight)/2}px`;
-            scrollingBackgroundParts[5].style.top = `${(backTop - lastScrollTop + 2*backheight)/3}px`;
+    if (lastScrollTop >= backTop) {
+        for (let i = 0; i < scrollingBackgroundParts.length; i++) {
+            if (i < 3) { scrollingBackgroundParts[i].style.opacity = `${(backTop - lastScrollTop + backheight) / (2 * backheight) + 0.5}`; } // calculating position - last window position + height of the block and dividing it by height of the block to get value from 1 to 0
+            else { scrollingBackgroundParts[i].style.opacity = `${(lastScrollTop - backTop - backheight) / (2 * backheight) + 0.5}`; }
+            scrollingBackgroundParts[1].style.top = `${(lastScrollTop - backTop) / 2}px`;
+            scrollingBackgroundParts[2].style.top = `${(lastScrollTop - backTop) / 3}px`;
+            scrollingBackgroundParts[4].style.top = `${(backTop - lastScrollTop + 2 * backheight) / 2}px`;
+            scrollingBackgroundParts[5].style.top = `${(backTop - lastScrollTop + 2 * backheight) / 3}px`;
         }
+    }
+    else if(lastScrollTop < backTop){
+        scrollingBackgroundParts[0].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
+        scrollingBackgroundParts[1].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
+        scrollingBackgroundParts[2].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
     }
     lastScrollTop = scrollTopPosition <= 0 ? 0 : scrollTopPosition;
 }
