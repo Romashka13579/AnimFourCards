693496cf2bfa865645b366a1e7ce2c68dbcafee1
Romashka <rroma13579@gmail.com>
2023-11-06 11:42:40 +0100

    some animations to my skies

--- js/scrolling-background.js
+++ js/scrolling-background.js
@@ -11,11 +11,13 @@ window.addEventListener('scroll', () => {
     const scrollTopPosition = document.documentElement.scrollTop;
 
     for (let i = 0; i < scrollingBackgroundParts.length; i++) {
-        if(i < 3){
-            scrollingBackgroundParts[i].style.opacity = `${(backTop - lastScrollTop + backheight)/backheight}`;
-        }
-        else{
-            scrollingBackgroundParts[i].style.opacity = `${(lastScrollTop - backTop - backheight)/backheight}`;
+        if(i < 3){scrollingBackgroundParts[i].style.opacity = `${(backTop - lastScrollTop + backheight)/backheight}`;}
+        else{scrollingBackgroundParts[i].style.opacity = `${(lastScrollTop - backTop - backheight)/backheight}`;}
+        if(lastScrollTop >= backTop){
+            scrollingBackgroundParts[1].style.top = `${(lastScrollTop - backTop)/2}px`;
+            scrollingBackgroundParts[2].style.top = `${(lastScrollTop - backTop)/3}px`;
+            scrollingBackgroundParts[4].style.top = `${(backTop - lastScrollTop + 2*backheight)/2}px`;
+            scrollingBackgroundParts[5].style.top = `${(backTop - lastScrollTop + 2*backheight)/3}px`;
         }
     }
     lastScrollTop = scrollTopPosition <= 0 ? 0 : scrollTopPosition;
