de93513ad334f3dd4ba863b5c053a12b8dfd28ad
Romashka <rroma13579@gmail.com>
2023-11-06 18:50:09 +0100

    adding two gradients to style it all a little bit

--- js/scrolling-background.js
+++ js/scrolling-background.js
@@ -1,6 +1,7 @@
 var scrollingBackgroundParts = document.querySelectorAll('.scrolling-background-part');
 var scrollingBackground = document.querySelector('.scrolling-background');
 var scrollingBackgroundMain = document.querySelector('.scrolling-background-main');
+var scrollingBackgroundPre = document.querySelector('.scrolling-background-pre');
 
 let lastScrollTop = document.documentElement.scrollTop;
 let backTop = scrollingBackground.offsetTop; // position of scrolling background in document = 7000+px
@@ -9,7 +10,7 @@ let backheight = scrollingBackgroundMain.getBoundingClientRect().height; // heig
 window.addEventListener('scroll', () => {
     const scrollTopPosition = document.documentElement.scrollTop;
 
-    if (lastScrollTop >= backTop) {
+    if (lastScrollTop >= backTop && (lastScrollTop - backTop) <= backheight*2) {
         for (let i = 0; i < scrollingBackgroundParts.length; i++) {
             if (i < 3) { scrollingBackgroundParts[i].style.opacity = `${(backTop - lastScrollTop + backheight) / (2 * backheight) + 0.5}`; } // calculating position - last window position + height of the block and dividing it by height of the block to get value from 1 to 0
             else { scrollingBackgroundParts[i].style.opacity = `${(lastScrollTop - backTop - backheight) / (2 * backheight) + 0.5}`; }
@@ -19,10 +20,14 @@ window.addEventListener('scroll', () => {
             scrollingBackgroundParts[5].style.top = `${(backTop - lastScrollTop + 2 * backheight) / 3}px`;
         }
     }
-    else if(lastScrollTop < backTop){
-        scrollingBackgroundParts[0].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
-        scrollingBackgroundParts[1].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
-        scrollingBackgroundParts[2].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
+    // else if(lastScrollTop < backTop){
+    //     scrollingBackgroundPre.style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
+    //     scrollingBackgroundParts[0].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
+    //     scrollingBackgroundParts[1].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
+    //     scrollingBackgroundParts[2].style.opacity = `${(backheight - backTop + lastScrollTop) / backheight}`;
+    // }
+    else if((lastScrollTop - backTop) > backheight*2){
+        console.log(lastScrollTop);
     }
     lastScrollTop = scrollTopPosition <= 0 ? 0 : scrollTopPosition;
 }
