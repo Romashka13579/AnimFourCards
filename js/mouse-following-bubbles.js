34939dc7a9923dddc82f84a5af158f08d919dc11
Romashka <rroma13579@gmail.com>
2023-12-06 12:26:43 +0100

    buttons block

--- js/mouse-following-bubbles.js
+++ js/mouse-following-bubbles.js
@@ -9,8 +9,8 @@ mousefollowingbubbles.addEventListener('mousemove', (e) => {
         var clone = mousefollowingbubble.cloneNode(true);
         clone.style.background = `rgb(${255 * Math.random()}, ${255 * Math.random()}, ${255 * Math.random()})`;
         clone.style.transform = `scale(${0.5 + 0.5 * Math.random()})`;
-        clone.style.top = `${y + 25 - 100 * Math.random()}px`;
-        clone.style.left = `${x + 25 - 100 * Math.random()}px`;
+        clone.style.top = `${y + 15 - 70 * Math.random()}px`;
+        clone.style.left = `${x + 15 - 70 * Math.random()}px`;
         clone.classList += ' bubble-clone';
         mousefollowingbubbles.append(clone);
     
