97783e94d8f625aaeeb17e151473958f91e6e14e
Romashka <rroma13579@gmail.com>
2023-12-18 22:46:34 +0100

    cool hovering cards, only design is left

--- js/item.js
+++ js/item.js
@@ -1,9 +1,11 @@
-var List = [document.querySelector('.item-card-hover')];
+var List = [document.querySelectorAll('.item-card'), document.querySelectorAll('.item-over')];
 
 window.addEventListener('mousemove', (e) => {
-    List.forEach(item => {
-        item.style.left = `${e.clientX - item.offsetWidth/2}px`;
-        item.style.top = `${e.clientY - item.offsetHeight/2}px`;
-        console.log(e.clientX);
+    List.forEach(itemlist => {
+        itemlist.forEach(item => {
+            hover = item.querySelector('.item-hover');
+            hover.style.left = `${e.clientX - hover.offsetWidth/2 - item.getBoundingClientRect().left}px`;
+            hover.style.top = `${e.clientY - hover.offsetHeight/2 - item.getBoundingClientRect().top}px`;
+        });
     });
 });
