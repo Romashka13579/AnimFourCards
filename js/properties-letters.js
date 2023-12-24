723485c70eb03c5126c57a3264d4f815ba86626d
Romashka <rroma13579@gmail.com>
2023-12-24 23:08:27 +0100

    jumping animation, need to work on making it more smooth

--- js/properties-letters.js
+++ js/properties-letters.js
@@ -1,7 +1,11 @@
 var jumpingLetters = document.querySelector('.jumping-letters');
 
-var letters = jumpingLetters.dataset.text.split();
-letters.forEach(letter => {
-    jumpingLetters.append(`${letter}`);
-});
-console.log(letters);
+var letters = jumpingLetters.dataset.text.split('');
+for (let i = 0; i < letters.length; i++) {
+    var newDiv = document.createElement("div");
+    newDiv.innerHTML = letters[i];
+    newDiv.classList.add('jumping-letter');
+    newDiv.style.animationDelay = `${(i * 4)/letters.length}s`;
+    jumpingLetters.append(newDiv);
+    console.log("a");
+}
