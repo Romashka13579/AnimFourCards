b8ebc633dfa3ea52474aeae5155c70baad72dfe7
Romashka <rroma13579@gmail.com>
2023-12-25 16:47:11 +0100

    added floating animation, but it has to be more smooth

--- js/properties-letters.js
+++ js/properties-letters.js
@@ -1,11 +1,24 @@
-var jumpingLetters = document.querySelector('.jumping-letters');
+var LettersBlocks = document.querySelectorAll('.letters-block');
 
-var letters = jumpingLetters.dataset.text.split('');
-for (let i = 0; i < letters.length; i++) {
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML = letters[i];
-    newDiv.classList.add('jumping-letter');
-    newDiv.style.animationDelay = `${(i * 4)/letters.length}s`;
-    jumpingLetters.append(newDiv);
-    console.log("a");
-}
+LettersBlocks.forEach(block => {
+    var letters = block.querySelector('.letters');
+    console.log(letters);
+    var lettersArray = letters.dataset.text.split('');
+    for (let i = 0; i < lettersArray.length; i++) {
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = lettersArray[i];
+        newDiv.classList.add('letter');
+        switch (letters.dataset.text) {
+            case "Jumping":
+                newDiv.classList.add('jumping');
+                newDiv.style.animationDelay = `${(i * 4) / lettersArray.length}s`;
+                break;
+            case "Floating":
+                newDiv.classList.add('floating');
+                newDiv.style.animationDelay = `${(i * 4) / lettersArray.length}s`;
+                break;
+        }
+        letters.append(newDiv);
+        console.log("a");
+    }
+});
