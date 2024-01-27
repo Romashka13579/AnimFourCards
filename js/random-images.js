57acfca4abe17b3e315afcb8f22bee5b14709795
Romashka <rroma13579@gmail.com>
2024-01-27 23:39:30 +0100

    trying to create random image slider

---
+++ js/random-images.js
@@ -0,0 +1,5 @@
+var images = document.querySelectorAll('.random-images-image');
+
+images.forEach(image => {
+    image.style.background = "url(/images/random-images/julian-lozano-FMZxjmeZ95o-unsplash.jpg) center/cover";
+});
