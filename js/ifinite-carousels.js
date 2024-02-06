26a5e29831acf3d960f6d13532b65cff89bf5654
Romashka <rroma13579@gmail.com>
2024-02-06 10:28:11 +0100

    mouse following bulb

--- js/ifinite-carousels.js
+++ js/ifinite-carousels.js
@@ -1,11 +1,14 @@
 var carousel = document.querySelector('.carousel');
+var carouselWrap = document.querySelector('.carousel-wrap');
 var carouselOver = document.querySelector('.carousel-over');
 
 var carouselItems = document.querySelectorAll('.carousel-item');
+var hoverball = document.querySelector('.carousel-hoverball');
 
 for (let j = 0; j < 5; j++) {
     for (let i = 0; i < carouselItems.length; i++) {
-        carouselOver.append(carouselItems[i].cloneNode(true));
+        var clone = carouselItems[i].cloneNode(true);
+        carouselOver.append(clone);
     }
 }
 
@@ -14,6 +17,8 @@ var carouselItems = document.querySelectorAll('.carousel-item');
 for (let i = 0; i < carouselItems.length; i++) {
     carouselItems[i].style.transform = `rotateY(${12 * i}deg) translateZ(-170vw)`;
 }
+
+var carouselBlocks = document.querySelectorAll('.carousel-block');
 
 var startDragging = false;
 var position = 0;
@@ -28,10 +33,22 @@ const dragstart = (e) => {
 }
 
 const dragging = (e) => {
-    if (!startDragging) return;
+    if (!startDragging) {
+        const x = e.clientX -100;
+        const y = e.pageY - carouselWrap.offsetTop - carousel.offsetTop -100;
+        const keyframes = {
+            left: `${x}px`,
+            top: `${y}px`
+        }
+        hoverball.animate(keyframes, {
+            duration: 1500,
+            fill: "forwards"
+        });
+        return;
+    };
     e.preventDefault();
     rotation = (position - parseInt(e.clientX)) / 14 + startRotation;
-    carouselOver.style.transform = `perspective(600px) translateZ(170vw) translateY(-30px) rotateZ(-4deg) rotateY(${rotation + scrolled}deg)`;
+    carouselOver.style.transform = `perspective(600px) translateZ(170vw) rotateZ(-4deg) rotateY(${rotation + scrolled}deg)`;
 }
 
 const dragover = (e) => {
@@ -43,12 +60,29 @@ const dragover = (e) => {
 const scroll = (e) => {
     e.preventDefault();
     scrolled = document.documentElement.scrollTop / 40;
-    carouselOver.style.transform = `perspective(600px) translateZ(170vw) translateY(-30px) rotateZ(-4deg) rotateY(${rotation + scrolled}deg)`;
+    carouselOver.style.transform = `perspective(600px) translateZ(170vw) rotateZ(-4deg) rotateY(${rotation + scrolled}deg)`;
 }
 
-carousel.addEventListener('mousemove', dragging);
-carousel.addEventListener('mousedown', dragstart);
-carousel.addEventListener('mouseup', dragover);
+// const mouseon = (e) => {
+//     e.preventDefault();
+//     console.log(e.target.boundingClientRect());
+// }
+
+const mouseon = (e) => {
+    e.preventDefault();
+    hoverball.style.display = "flex";
+    setTimeout(() => {
+        hoverball.style.opacity = 1;
+    }, 10);
+}
+
+carouselWrap.addEventListener('mousemove', dragging);
+carouselWrap.addEventListener('mousedown', dragstart);
+carouselWrap.addEventListener('mouseup', dragover);
+carouselWrap.addEventListener('mouseover', mouseon);
+// carouselItems.forEach(item => {
+//     item.addEventListener('mouseover', mouseon);
+// });
 
 window.addEventListener('scroll', scroll);
 
