f8e317fe35ce018dbf8db4408b7fae7325848230
Romashka <rroma13579@gmail.com>
2024-02-25 20:39:34 +0100

    main page parallax + updated parallax

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var mainParallaxLayers = document.querySelectorAll('.main-parallax-layer');
+var paralaxMain = document.querySelector('.main-parallax');
+var paralaxMainClient = paralaxMain.getBoundingClientRect();
+var y = 0;
+var y1 = 0;
+var y2 = 0;
+var x1 = 0;
+var x = 0;
+
+
+Start();
+
+function Start() {
+    for (let i = 0; i < mainParallaxLayers.length; i++) {
+        setTimeout(() => {
+            mainParallaxLayers[i].style.marginTop = `-20vh`;
+            Moving(mainParallaxLayers[i]);
+        }, 200*i/Math.pow(1.2,i));
+    }
+}
+
+function Moving(object){
+    window.addEventListener('mousemove', (e) => {
+        paralaxMain = document.querySelector('.main-parallax');
+        paralaxMainClient = paralaxMain.getBoundingClientRect();
+        if(parseFloat(paralaxMainClient.height) < e.pageY) return;
+        x1 = e.clientX;
+        y1 = e.clientY;
+        x = (20 - x1/paralaxMainClient.width * 40) * parseFloat(object.dataset.speedx);
+        y = (0 - (y1 + y2)/paralaxMainClient.height * 20) * parseFloat(object.dataset.speedy);
+        object.style.marginTop = `-20vh`;
+        object.style.transform = `translateX(${x}vw) translateY(${y}vh)`;
+    });
+
+    window.addEventListener('scroll', () => {
+        y2 = window.scrollY*2.5;
+        y = (0 - (y1 + y2)/paralaxMainClient.height * 20) * parseFloat(object.dataset.speedy);
+        x = (20 - x1/paralaxMainClient.width * 40) * parseFloat(object.dataset.speedx);
+        object.style.marginTop = `-20vh`;
+        object.style.transform = `translateX(${x}vw) translateY(${y}vh)`;
+    });
+}
